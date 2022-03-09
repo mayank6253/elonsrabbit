04dd25c6df24afacb43465d4aa06e187aa595756
mayank6253 <89696473+mayank6253@users.noreply.github.com>
2022-03-09 14:48:46 +0530

    elons rabbit updated

--- src/components/HerosSection.js
+++ src/components/HerosSection.js
@@ -4,12 +4,102 @@ import headerimage from "../images/headerimage (2).png";
 import cmc from "../images/cmc.png";
 import cg from "../images/cg.png";
 import bsc from "../images/bsc.png";
-import Roadmap from "./Roadmap";
 import { AiOutlineTwitter } from "react-icons/ai";
 import { FaTelegram } from "react-icons/fa";
 import rabbit from "../images/rabbit (2).png";
+import Roadmap from "./Roadmap";
 
 export default function HerosSection() {
+  // const timelineData = [
+  //   {
+  //     text: "Started working on the app-ideas repository",
+  //     date: "February 25 2019",
+  //     category: {
+  //       tag: "app-ideas",
+  //       color: "#FFDB14",
+  //     },
+  //     link: {
+  //       url: "https://github.com/florinpop17/app-ideas",
+  //       text: "Check it out on GitHub",
+  //     },
+  //   },
+  //   {
+  //     text: "Started the Weekly Coding Challenge program",
+  //     date: "March 04 2019",
+  //     category: {
+  //       tag: "blog",
+  //       color: "#e17b77",
+  //     },
+  //     link: {
+  //       url: "https://florin-pop/blog/2019/03/weekly-coding-challenge/",
+  //       text: "Check it out here",
+  //     },
+  //   },
+  //   {
+  //     text: "Got 1.000 followers on Twitter",
+  //     date: "March 07 2019",
+  //     category: {
+  //       tag: "twitter",
+  //       color: "#1DA1F2",
+  //     },
+  //     link: {
+  //       url: "https://twitter.com/florinpop1705",
+  //       text: "See profile",
+  //     },
+  //   },
+  //   {
+  //     text: "I published my first article in the FreeCodeCamp Medium Publication",
+  //     date: "March 18 2019",
+  //     category: {
+  //       tag: "medium",
+  //       color: "#018f69",
+  //     },
+  //     link: {
+  //       url: "https://medium.freecodecamp.org/how-to-build-a-double-slider-sign-in-and-sign-up-form-6a5d03612a34",
+  //       text: "Check it out here",
+  //     },
+  //   },
+  //   {
+  //     text: "Over 12.000 views in a single day on my Medium posts",
+  //     date: "April 05 2019",
+  //     category: {
+  //       tag: "medium",
+  //       color: "#018f69",
+  //     },
+  //     link: {
+  //       url: "https://medium.com/@popflorin1705",
+  //       text: "See profile",
+  //     },
+  //   },
+  // ];
+
+  // const TimelineItem = ({ data }) => (
+  //   <div className="timeline-item">
+  //     <div className="timeline-item-content">
+  //       <span className="tag" style={{ background: data.category.color }}>
+  //         {data.category.tag}
+  //       </span>
+  //       <time>{data.date}</time>
+  //       <p>{data.text}</p>
+  //       {data.link && (
+  //         <a href={data.link.url} target="_blank" rel="noopener noreferrer">
+  //           {data.link.text}
+  //         </a>
+  //       )}
+  //       <span className="circle" />
+  //     </div>
+  //   </div>
+  // );
+
+  // const Timeline = () =>
+  //   timelineData.length > 0 && (
+  //     <div className="timeline-container">
+  //       {timelineData.map((data, idx) => (
+  //         <TimelineItem data={data} key={idx} />
+  //       ))}
+  //     </div>
+  //   );
+
   return (
     <>
       {/* HOME */}
@@ -43,119 +133,145 @@ export default function HerosSection() {
 
       <section>
         <div className="container">
-          <h2 id="roadmap">RoadMap</h2>
+          <h2 id="roadmap-cont">RoadMap</h2>
           <Roadmap />
         </div>
       </section>
 
       {/* TOKENS */}
 
-      <section className="token-container">
-        <div className="token-content">
-          <h2 id="token">Tokenomics</h2>
-          <p className="token-text">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            <br /> Atque laboriosam sed rem optio maiores eveniet a ab
-            repudiandae facere ex!
-          </p>
-        </div>
-        <section className="tokencontent-container">
-          <div className="row d-flex align-items-end">
-            <div className="area-block col-xl-4 col-md-6 col-12">
-              <div className="info-listing">
-                <div className="info-block">
-                  <div className="title">
-                    <span className="percent">5%</span>
-                    <span className="dot"></span>
-                    <span className="count">Airdrop</span>
-                  </div>
-                  <div className="desc">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
-                    accusantium pariatur tenetur obcaecati ullam. Ratione?
-                  </div>
-                </div>
-
-                <div className="info-block">
-                  <div className="title">
-                    <span className="percent">50%</span>
-                    <span className="dot"></span>
-                    <span className="count">Luquidity</span>
-                  </div>
-                  <div className="desc">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Quaerat nisi consequuntur itaque voluptatibus commodi
-                    fugiat.
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="area-block tokenemics-block col-xl-4 col-12">
-              <div className="tokenomics-circle">
-                <div className="circle-block marketing">
-                  <div className="content">
-                    <span>5%</span> Airdrop
-                  </div>
-                </div>
-
-                <div className="circle-block team">
-                  <div className="content">
-                    <span>10%</span> IDO
-                  </div>
-                </div>
-
-                <div className="circle-block liquidity">
-                  <div className="content">
-                    <span>30%</span> Staking Rewards
-                  </div>
-                </div>
-
-                <div className="circle-block airdrop">
-                  <div className="content">
-                    <span>50%</span> CEX/DEX
-                  </div>
-                </div>
-              </div>
-
-              <div className="tokenomics-total">
-                <div className="title">Total Supply</div>
-                <div className="value">1,00,000,00</div>
-                <div className="name">ELONS RABBIT</div>
-              </div>
-            </div>
-
-            <div className="area-block col-xl-4 col-md-6 col-12">
-              <div className="info-listing">
-                <div className="info-block">
-                  <div className="title">
-                    <span className="percent">15%</span>
-                    <span className="dot"></span>
-                    <span className="count">Allocation for CEX/DEX</span>
-                  </div>
-                  <div className="desc">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Assumenda earum amet sed? Libero, architecto voluptatem.
-                  </div>
-                </div>
-
-                <div className="info-block">
-                  <a href="/" className="info-link">
-                    <div className="title">
-                      <span className="percent">30%</span>
-                      <span className="dot"></span>
-                      <span className="count">Staking Rewards</span>
+      <div id="tokenomics" className="row">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <div className="token-area">
+                <div className="row">
+                  <div className="col-12">
+                    <div className="section-header">
+                      <div className="section-title">Tokenomics</div>
+                      <div className="section-desc">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing
+                        elit.
+                      </div>
                     </div>
-                    <div className="desc">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Minus magnam sed sit delectus. Laudantium, similique!
+
+                    <div className="section-content">
+                      <div className="row align-items-end">
+                        <div className="area-block col-xl-4 col-md-6 col-12">
+                          <div className="info-listing">
+                            <div className="info-block">
+                              <div className="title">
+                                <span className="percent">5%</span>
+                                <span className="dot"></span>
+                                <span className="count">Airdrop</span>
+                              </div>
+                              <div className="desc">
+                                Lorem ipsum dolor sit amet consectetur,
+                                adipisicing elit. Debitis quia aliquid error
+                                porro optio repudiandae, possimus sit laborum
+                                natus beatae, quibusdam libero, facere molestias
+                                aut esse fugit in doloribus corrupti.
+                              </div>
+                            </div>
+
+                            <div className="info-block">
+                              <div className="title">
+                                <span className="percent">50%</span>
+                                <span className="dot"></span>
+                                <span className="count">Luquidity</span>
+                              </div>
+                              <div className="desc">
+                                Lorem ipsum dolor sit amet consectetur,
+                                adipisicing elit. Debitis quia aliquid error
+                                porro optio repudiandae, possimus sit laborum
+                                natus beatae, quibusdam libero, facere molestias
+                                aut esse fugit in doloribus corrupti.
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+
+                        <div className="area-block tokenemics-block col-xl-4 col-12">
+                          <div className="tokenomics-circle">
+                            <div className="circle-block marketing">
+                              <div className="content">
+                                <span>5%</span> Airdrop
+                              </div>
+                            </div>
+
+                            <div className="circle-block team">
+                              <div className="content">
+                                <span>10%</span> IDO
+                              </div>
+                            </div>
+
+                            <div className="circle-block liquidity">
+                              <div className="content">
+                                <span>30%</span> Staking Rewards
+                              </div>
+                            </div>
+
+                            <div className="circle-block airdrop">
+                              <div className="content">
+                                <span>50%</span> CEX/DEX
+                              </div>
+                            </div>
+                          </div>
+
+                          <div className="tokenomics-total">
+                            <div className="title">Total Supply</div>
+                            <div className="value">1,000,000</div>
+                            <div className="name">XYZ</div>
+                          </div>
+                        </div>
+
+                        <div className="area-block col-xl-4 col-md-6 col-12">
+                          <div className="info-listing">
+                            <div className="info-block">
+                              <div className="title">
+                                <span className="percent">15%</span>
+                                <span className="dot"></span>
+                                <span className="count">
+                                  Allocation for CEX/DEX
+                                </span>
+                              </div>
+                              <div className="desc">
+                                Lorem ipsum dolor sit amet consectetur,
+                                adipisicing elit. Debitis quia aliquid error
+                                porro optio repudiandae, possimus sit laborum
+                                natus beatae, quibusdam libero, facere molestias
+                                aut esse fugit in doloribus corrupti.
+                              </div>
+                            </div>
+
+                            <div className="info-block">
+                              <a href="/" className="block">
+                                <div className="title">
+                                  <span className="percent">30%</span>
+                                  <span className="dot"></span>
+                                  <span className="count">Staking Rewards</span>
+                                </div>
+                                <div className="desc">
+                                  Lorem ipsum dolor sit amet consectetur,
+                                  adipisicing elit. Debitis quia aliquid error
+                                  porro optio repudiandae, possimus sit laborum
+                                  natus beatae, quibusdam libero, facere
+                                  molestias aut esse fugit in doloribus
+                                  corrupti.
+                                </div>
+                              </a>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
                     </div>
-                  </a>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </section>
-      </section>
+        </div>
+      </div>
 
       {/*EXCHANGE */}
 
@@ -166,10 +282,10 @@ export default function HerosSection() {
               <h2 id="exchange">Patners</h2>
               <div className="partners-images">
                 <a
-                  href="https://coinmarketcap.com/currencies/elonsrabbit/" 
-                  target="_blank"  rel="noreferrer"
+                  href="https://coinmarketcap.com/currencies/elonsrabbit/"
+                  target="_blank"
+                  rel="noreferrer"
                   className="er-link"
-                
                 >
                   <div className="coinMraketCap">
                     <img src={cmc} alt="" className="mgb" /> <br />
@@ -178,7 +294,8 @@ export default function HerosSection() {
                 </a>
                 <a
                   href="https://www.coingecko.com/en/coins/elons-rabbit"
-                  target="_blank"  rel="noreferrer"
+                  target="_blank"
+                  rel="noreferrer"
                   className="er-link"
                 >
                   <div className="coingecko">
@@ -188,7 +305,8 @@ export default function HerosSection() {
                 </a>
                 <a
                   href="https://bscscan.com/token/0x92a4EbEE814AFe58741d7f216dC10211d5aBC250"
-                  target="_blank"  rel="noreferrer"
+                  target="_blank"
+                  rel="noreferrer"
                   className="er-link"
                 >
                   <div className="coinMraketCap">
--- src/components/Roadmap.js
+++ src/components/Roadmap.js
@@ -1,10 +1,77 @@
 import React from "react";
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import {GiMissileLauncher, GiRocketFlight, GiPoliceBadge, GiMetalBoot} from 'react-icons/gi'
 
 export default function Roadmap() {
- 
-   return(
-     <>
-       hello
-     </>
-   );
+
+  return (
+    <div id="roadmap-cont">
+      <VerticalTimeline>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+          date="Dec 2021 - Feb 2022"
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          icon={<GiMissileLauncher />}
+        >
+          <h3 className="vertical-timeline-element-title">PHASE I</h3>
+          <h4 className="vertical-timeline-element-subtitle">PRE-LAUNCH</h4>
+          <p>
+          • Private Sale to collect funds for development of platform &AMP; presale marketing<br/>
+          • Beta Version of $BBT platform with "Safe Haven"<br/>
+          • Presale Marketing and promotions<br/>
+          • Listing of 100+ Project Owners, Developers, Influencers/Promoters
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+          date="March 2022"
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          icon={<GiRocketFlight />}
+        >
+          <h3 className="vertical-timeline-element-title">PHASE II</h3>
+          <h4 className="vertical-timeline-element-subtitle">POST LAUNCH</h4>
+          <p>
+          • V1 of $BBT platform launch<br/>
+          • Listing of more Project Owners, Developers, Influencers/Promoters<br/>
+          • Post launch marketing campaign<br/>
+          • Major Listings &amp; Tendings
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="April 2022"
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          icon={<GiPoliceBadge />}
+        >
+          <h3 className="vertical-timeline-element-title">PHASE III</h3>
+          <h4 className="vertical-timeline-element-subtitle">INSURING DEFI</h4>
+          <p>
+          • Launch of Insurance on blockchain for $BBT holders<br/>
+          • Marketing campaign for Insurance platform<br/>
+          • Partnerships with major Insurance players
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="Jun - Jul 2022"
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          icon={<GiMetalBoot />}
+        >
+          <h3 className="vertical-timeline-element-title">PHASE IV</h3>
+          <h4 className="vertical-timeline-element-subtitle"></h4>
+          <p>
+          • Launchpad for projects willing to list on $BBT platform<br/>
+          • VC Funding and Presale platforms<br/>
+          • NFT Marketplace to be launched in Metaverse<br/>
+          • First Insurance provider in Metaverse
+          </p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
+  );
 }
